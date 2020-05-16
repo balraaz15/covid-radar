76f87e45da3cfca2b63978adc4dda4997eef648f
balraaz15 <balraaz11@gmail.com>
2020-05-16 19:17:36 +0545

    changed implementation of table in details page

--- src/components/Continents/Continent.jsx
+++ src/components/Continents/Continent.jsx
@@ -18,7 +18,7 @@ const Continent = props => {
 		<Fragment>
 			<table className={cx('bp3-html-table', 'bp3-html-table-striped', 'bp3-elevation-2', styles.table)}>
 				<thead>
-					<tr>
+					<tr className={styles.tableHeading}>
 						<th>Rank</th>
 						<th>Country</th>
 						<th>Total Cases</th>
@@ -35,20 +35,30 @@ const Continent = props => {
 				<tbody>
 					{
 						props.data ?
-							props.data.map((d, i) => <tr key={d.country}>
+							props.data.map((d, i) => <tr key={d.country} className={styles.countryRow}>
 								<td>{i + 1}</td>
 								<td title={d.country}>
 									<img src={d.countryInfo.flag} width="20" className={styles.flag} alt={d.country} />
 									{d.country.length > 15 ? d.country.slice(0, 15) + '...' : d.country}
 								</td>
 								<td>{numberWithCommas(d.cases)}</td>
-								<td>{numberWithCommas(d.todayCases)}</td>
+								<td className={d.todayCases ? styles.todayCases : ''}>
+									{d.todayCases ? '+' + numberWithCommas(d.todayCases) : ''}
+								</td>
 								<td>{numberWithCommas(d.deaths)}</td>
-								<td>{numberWithCommas(d.todayDeaths)}</td>
-								<td>{numberWithCommas(d.recovered)}</td>
-								<td>{numberWithCommas(d.active)}</td>
+								<td className={d.todayDeaths ? styles.todayDeaths : ''}>
+									{d.todayDeaths ? '+' + numberWithCommas(d.todayDeaths) : ''}
+								</td>
+								<td>
+									{d.recovered ? numberWithCommas(d.recovered) : <span className={styles.notAvailable}>N/A</span>}
+								</td>
+								<td>
+									{d.active ? numberWithCommas(d.active) : <span className={styles.notAvailable}>N/A</span>}
+								</td>
 								<td>{numberWithCommas(d.critical)}</td>
-								<td>{numberWithCommas(d.tests)}</td>
+								<td>
+									{d.tests ? numberWithCommas(d.tests) : <span className={styles.notAvailable}>N/A</span>}
+								</td>
 								<td>{numberWithCommas(d.population)}</td>
 							</tr>) : <Spinner />
 					}
